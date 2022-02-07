b04fdb96de91e49bb797db495396979c38aad63e
saiphyozinko <40524304@live.napier.ac.uk>
2022-02-07 12:41:00 +0630

    generate days from calendar module

--- server.py
+++ server.py
@@ -1,9 +1,48 @@
 from flask import Flask, render_template
+import calendar
+
+# <date>
 
 app = Flask(__name__)
 
-@app.route("/lionking/<date>")
-def calendar(date):
+@app.route("/")
+def index():
+    # return render_template("calendar.html")
     return render_template("calendar.html")
 
-app.run(debug=True)
+
+@app.route("/lionking/<date>")
+def data (date):
+    # year = 2021
+    # month = 1
+    print(date)
+    
+    [year,month] = date.split('-')
+    year = int(year)
+    month = int(month)
+    print(year)
+    print(month)
+    days = []
+    monthName = []
+    dates = []
+    data = calendar.TextCalendar(calendar.MONDAY)
+    for i in data.itermonthdays(year,month):
+        days.append(i)
+    for name in calendar.month_name:
+        monthName.append(name)
+    calData = calendar.Calendar()
+    dateName = calData.itermonthdates(year,month)
+    for d in dateName:
+        dates.append(str(d))
+    resultDate = {
+        'monthYear' :   year,
+        'monthName' :   monthName[month],
+        'month'     :   month,
+        'days'      :   days,
+        'dates'     :   dates
+    }
+    
+    return resultDate
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- static/lionking.js
+++ static/lionking.js
@@ -1,4 +1,4 @@
-//Create a table to show every day in Feb
+// Create a table to show every day in Feb
 let monthHTML = `
 <table>
  <caption>February</caption>
@@ -6,7 +6,7 @@ let monthHTML = `
  <th>Mon</th>
  <th>Tue</th>
  <th>Wed</th>
- <th>Thu</th>
+ <th>Thu</th>   
  <th>Fri</th>
  <th>Sat</th>
  <th>Sun</th>
@@ -32,25 +32,102 @@ let monthHTML = `
 
 </table>
 `;
-document.getElementById('calendar').innerHTML = monthHTML;
+// jQuery.get("/lionking/2021-02-02",function(data){
+//   let x = JSON.parse(data)
+//   console.log(x)
+// })
+//   .then((r) => r.json())
+//   .then((r) => {
+//     console.log(r)
+//     for(let d of r){
+//       console.log(d.days)
+//     }
 
-fetch('https://tw.igs.farm/lionking/all.json')
-  .then(r=>r.json())
-  .then(r=>{
-      let performances = r.data.getShow.show.performances;
-      for(let p of performances){
-          let pd = p.dates.performanceDate;
-          let cellId = "d"+pd.split("T")[0];
-          let cell = document.getElementById(cellId);
-          if (cell !== null){
-              let performanceDiv = document.createElement('div');
-              performanceDiv.classList.add(p.performanceTimeDescription);
-              let line1 = document.createElement('div');
-              line1.innerHTML = `<div class='dot ${p.availabilityStatus}'></div> ${pd.substr(11,5)}`;
-              let line2 = document.createElement('div');
-              line2.innerHTML = 'tbc';
-              performanceDiv.append(line1,line2);
-              cell.append(performanceDiv);
-          }
-      }
+// })
+// for (let day = 1; day < 31; day ++){
+//     console.log(day)
+//     let monthHTML =`<div class="day">${day}</div>`;
+
+// }
+// let data = JSON.parse("{{ result }}");
+
+// console.log(data)
+// year = 2021
+// month = 02
+yearmonth = '2022-01'
+
+fetch(`/lionking/${yearmonth}`)
+  .then(r => r.json())
+  .then(r => {
+    console.log(r.days)
+  // console.log(r.dates)
+  let dayName = `
+  <tr>
+  <th>Mon</th>
+  <th>Tue</th>
+  <th>Wed</th>
+  <th>Thu</th>
+  <th>Fri</th>
+  <th>Sat</th>
+  <th>Sun</th>
+  </tr>
+  `
+  for (let d in r.days){
+    console.log(d)
+  }
+  document.getElementById("tab").innerHTML = dayName;
   })
+
+
+
+// document.getElementById("calendar").innerHTML = monthHTML;
+for (let td of document.querySelectorAll("td")) {
+  let daynum = td.innerText;
+  td.innerHTML = `<div class=box><div class=daynum>${daynum}</div><div id=availabilty></div></div>`;
+}
+
+fetch("https://tw.igs.farm/lionking/all.json")
+  .then((r) => r.json())
+  .then((r) => {
+    let performances = r.data.getShow.show.performances;
+
+    for (let p of performances) {
+      let pd = p.dates.performanceDate;
+      let cellId = "d" + pd.split("T")[0];
+      let cell = document.getElementById(cellId);
+      if (cell !== null) {
+        let performanceDiv = document.createElement("div");
+        performanceDiv.classList.add(p.performanceTimeDescription);
+        let line1 = document.createElement("div");
+        line1.innerHTML = `<div class='dot ${
+          p.availabilityStatus
+        }'></div> ${pd.substr(11, 5)}`;
+        let line2 = document.createElement("div");
+        line2.classList.add("price");
+        line2.innerHTML = `from £${p.price.minPrice}`;
+        performanceDiv.append(line1, line2);
+        performanceDiv.onclick = () => {
+          document.getElementById("calendar").classList.add("hide");
+          console.log("I should be getting data for: ", p.id);
+          fetch(
+            "https://tw.igs.farm/lionking/692A2B01-A607-4616-9409-9696D905340D"
+          )
+            .then((r) => r.json())
+            .then((r) => {
+              for (let s of r.seats) {
+                let d = document.createElement("div");
+                d.classList.add("seat");
+                d.classList.add("Z" + s.zone);
+                d.style.left = s.x / 2 + 500 + "px";
+                d.style.top = s.y / 2 + 200 + "px";
+                if (s.available) {
+                  d.classList.add("available");
+                }
+                document.getElementById("seats").append(d);
+              }
+            });
+        };
+        cell.querySelector(".box").append(performanceDiv);
+      }
+    }
+  });
