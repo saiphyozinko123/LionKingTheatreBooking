53d29bd405a8dfd2036b8fda32cb8e85820b9e25
saiphyozinko <40524304@live.napier.ac.uk>
2022-02-08 18:00:39 +0630

    change datetime modulde from calendar module to get require data and dynamic calendar layout

--- server.py
+++ server.py
@@ -1,48 +1,100 @@
-from flask import Flask, render_template
-import calendar
+from flask import Flask, render_template,redirect
+from datetime import datetime
+from datetime import timedelta
+
+# import calendar
 
 # <date>
 
 app = Flask(__name__)
 
+# @app.route("/")
+# def indexone():
+#     return redirect('/lionking/')
+
 @app.route("/")
 def index():
-    # return render_template("calendar.html")
+    
     return render_template("calendar.html")
 
 
-@app.route("/lionking/<date>")
-def data (date):
-    # year = 2021
-    # month = 1
-    print(date)
+# @app.route("/lionking/<date>")
+# def data (date):
+#     # year = 2021
+#     # month = 1
+#     print(date)
     
-    [year,month] = date.split('-')
+#     [year,month] = date.split('-')
+#     year = int(year)
+#     month = int(month)
+#     print(year)
+#     print(month)
+#     days = []
+#     monthName = []
+#     dates = []
+#     data = calendar.TextCalendar(calendar.MONDAY)
+#     for i in data.itermonthdays(year,month):
+#         days.append(i)
+#     for name in calendar.month_name:
+#         monthName.append(name)
+#     calData = calendar.Calendar()
+#     dateName = calData.itermonthdates(year,month)
+#     for d in dateName:
+#         dates.append(str(d))
+#     resultDate = {
+#         'monthYear' :   year,
+#         'monthName' :   monthName[month],
+#         'month'     :   month,
+#         'days'      :   days,
+#         'dates'     :   dates
+#     }
+    
+#     return resultDate
+
+
+
+
+
+# from datetime import time
+# from datetime import date
+# from datetime import timedelta
+
+@app.route("/lionking/<dateinfo>")
+def datainformation (dateinfo):
+    
+    [year,month,day] = dateinfo.split('-')
     year = int(year)
     month = int(month)
-    print(year)
-    print(month)
-    days = []
-    monthName = []
-    dates = []
-    data = calendar.TextCalendar(calendar.MONDAY)
-    for i in data.itermonthdays(year,month):
-        days.append(i)
-    for name in calendar.month_name:
-        monthName.append(name)
-    calData = calendar.Calendar()
-    dateName = calData.itermonthdates(year,month)
-    for d in dateName:
-        dates.append(str(d))
+    day = int(day)
+    dates = datetime(year,month,day)
+    startday = dates - timedelta(dates.weekday())
+    allweeks = []
+    for weeks in range(6):
+        weeksquad = []
+        for week in range(7):
+            # dateonly = datetime.strptime(startday+timedelta(week+7*weeks), '%a %b %d %Y').strftime('%d/%m/%Y')
+            # print(dateonly)
+            weeksquad.append(startday+timedelta(week+7*weeks))
+        allweeks.append(weeksquad)
+
+    # for w in weeks:
+        # print(w)
+        # for days in w:
+            # print(days)
+            # daysList.append(days)
+
     resultDate = {
-        'monthYear' :   year,
-        'monthName' :   monthName[month],
-        'month'     :   month,
-        'days'      :   days,
-        'dates'     :   dates
+        'monthYear' : year,
+        'monthName' : dates.strftime("%B"),
+        'week' : allweeks
+        # 'month' : month
+
     }
     
     return resultDate
+   
+    
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/lionking.js
+++ static/lionking.js
@@ -54,80 +54,129 @@ let monthHTML = `
 // console.log(data)
 // year = 2021
 // month = 02
-yearmonth = '2022-01'
 
-fetch(`/lionking/${yearmonth}`)
+let dateTime = new Date();
+let realDateTime = dateTime.getFullYear()+'-'+(dateTime.getMonth()+1)+'-'+'1';
+// let realDateTime = dateTime.getFullYear()+'-'+(dateTime.getMonth()+1)+'-'+dateTime.getDate();
+// console.log(realDateTime)
+// realDateTime = 2022-01-01
+
+fetch(`/lionking/${realDateTime}`)
   .then(r => r.json())
   .then(r => {
-    console.log(r.days)
-  // console.log(r.dates)
-  let dayName = `
-  <tr>
-  <th>Mon</th>
-  <th>Tue</th>
-  <th>Wed</th>
-  <th>Thu</th>
-  <th>Fri</th>
-  <th>Sat</th>
-  <th>Sun</th>
-  </tr>
-  `
-  for (let d in r.days){
-    console.log(d)
-  }
-  document.getElementById("tab").innerHTML = dayName;
+    // console.log(r.week)
+    let dayName = `
+    <tr>
+    <th>Mon</th>
+    <th>Tue</th>
+    <th>Wed</th>
+    <th>Thu</th>
+    <th>Fri</th>
+    <th>Sat</th>
+    <th>Sun</th>
+    </tr>
+    `
+    document.getElementById('tab').innerHTML = dayName;
+    for (let week of r.week ){
+      let row = document.createElement('tr');
+      // console.log(week)
+      
+      for (let eachDay of week){
+
+        function changeDateFormat(date) {
+          let changeDate = new Date(date),
+            month = ("0" + (changeDate.getMonth() + 1)).slice(-2),
+            day = ("0" + changeDate.getDate()).slice(-2);
+          return [changeDate.getFullYear(), month, day].join("-");
+        }
+        
+        // console.log(changeDateFormat(eachDay))
+        let weekDays = changeDateFormat(eachDay)
+        // console.log(weekDays)
+        let cell = document.createElement('td');
+        // cell.id = 'd' + weekDays
+        idName = 'd'+ weekDays
+        console.log(idName)
+        cell.setAttribute('id', idName);
+        cell.innerText = weekDays.split('-')[2]
+        // console.log(weekDays.split('-')[2])
+
+        // console.log(eachDay)
+        // c = eachDay.split(' ')[1]
+        // console.log(c)
+        // document.getElementById('tab').innerHTML = cell
+        row.append(cell)
+      }
+      // document.getElementById('tab').append(row)
+      // console.log(row)
+      document.getElementById('tab').append(row)
+    }
+    
+    // document.getElementById("tab").append(row)
+    // document.getElementById("tab").innerHTML = dayName;
+    
+
+    // document.getElementById("calendar").innerHTML = monthHTML;
+    for (let td of document.querySelectorAll("td")) {
+      let daynum = td.innerText;
+      td.innerHTML = `<div class=box><div class=daynum>${daynum}</div></div>`;
+    }
+
+    fetch("https://tw.igs.farm/lionking/all.json")
+    .then((r) => r.json())
+    .then((r) => {
+      let performances = r.data.getShow.show.performances;
+      
+  
+      for (let p of performances) {
+        let pd = p.dates.performanceDate;
+        let cellId = "d" + pd.split("T")[0];
+        let cell = document.getElementById(cellId);
+        if (cell !== null) {
+          let performanceDiv = document.createElement("div");
+          performanceDiv.setAttribute('id', 'maindiv');
+          performanceDiv.classList.add(p.performanceTimeDescription);
+          let line1 = document.createElement("div");
+          line1.setAttribute('id', 'firstdiv');
+          line1.innerHTML = `<div class='dot ${p.availabilityStatus}'></div> ${pd.substr(11, 5)}`;
+          let line2 = document.createElement("div");
+          line2.setAttribute('id', 'secdiv');
+          line2.classList.add("price");
+          line2.innerHTML = `from £${p.price.minPrice + p.price.minPriceFee}`;
+          performanceDiv.append(line1, line2);
+          
+          performanceDiv.onclick = () => {
+            document.getElementById("tab").classList.add("hide");
+            
+            console.log("I should be getting data for: ", p.id);
+            fetch(
+              `https://tw.igs.farm/lionking/${p.id}`
+            )
+              .then((r) => r.json())
+              .then((r) => {
+                for (let s of r.seats) {
+                  let d = document.createElement("div");
+                  d.classList.add("seat");
+                  d.classList.add("Z" + s.zone);
+                  d.style.left = s.x / 2 + 500 + "px";
+                  d.style.top = s.y / 2 + 200 + "px";
+                  if (s.available) {
+                    d.classList.add("available");
+                  }
+                  document.getElementById("seats").append(d);
+                }
+              });
+          };
+          cell.querySelector(".box").append(performanceDiv);
+          
+          // document.getElementById('box').append(performanceDiv);
+  
+  
+        }
+      }
+    });
   })
 
 
+ 
 
-// document.getElementById("calendar").innerHTML = monthHTML;
-for (let td of document.querySelectorAll("td")) {
-  let daynum = td.innerText;
-  td.innerHTML = `<div class=box><div class=daynum>${daynum}</div><div id=availabilty></div></div>`;
-}
-
-fetch("https://tw.igs.farm/lionking/all.json")
-  .then((r) => r.json())
-  .then((r) => {
-    let performances = r.data.getShow.show.performances;
-
-    for (let p of performances) {
-      let pd = p.dates.performanceDate;
-      let cellId = "d" + pd.split("T")[0];
-      let cell = document.getElementById(cellId);
-      if (cell !== null) {
-        let performanceDiv = document.createElement("div");
-        performanceDiv.classList.add(p.performanceTimeDescription);
-        let line1 = document.createElement("div");
-        line1.innerHTML = `<div class='dot ${
-          p.availabilityStatus
-        }'></div> ${pd.substr(11, 5)}`;
-        let line2 = document.createElement("div");
-        line2.classList.add("price");
-        line2.innerHTML = `from £${p.price.minPrice}`;
-        performanceDiv.append(line1, line2);
-        performanceDiv.onclick = () => {
-          document.getElementById("calendar").classList.add("hide");
-          console.log("I should be getting data for: ", p.id);
-          fetch(
-            "https://tw.igs.farm/lionking/692A2B01-A607-4616-9409-9696D905340D"
-          )
-            .then((r) => r.json())
-            .then((r) => {
-              for (let s of r.seats) {
-                let d = document.createElement("div");
-                d.classList.add("seat");
-                d.classList.add("Z" + s.zone);
-                d.style.left = s.x / 2 + 500 + "px";
-                d.style.top = s.y / 2 + 200 + "px";
-                if (s.available) {
-                  d.classList.add("available");
-                }
-                document.getElementById("seats").append(d);
-              }
-            });
-        };
-        cell.querySelector(".box").append(performanceDiv);
-      }
-    }
-  });
