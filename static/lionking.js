3e622897f09cb1b7fa11d9818f9a165963522a09
saiphyozinko <40524304@live.napier.ac.uk>
2022-02-19 15:16:11 +0630

    seat label ABCD

--- static/lionking.js
+++ static/lionking.js
@@ -113,8 +113,7 @@ function calendar(date){
     .then((r) => r.json())
     .then((r) => {
       let performances = r.data.getShow.show.performances;
-      
-  
+
       for (let p of performances) {
         let pd = p.dates.performanceDate;
         let cellId = "d" + pd.split("T")[0];
@@ -157,43 +156,96 @@ function calendar(date){
               .then((r) => {
                 let thelist = []
                 let total = 0;
+                // for (let i of Object.values(r)){
+                //   console.log(i)
+                // }
+                // for(let h of r.zones){
+
+                // }
+                // for(let y of r.seats){
+                //   console.log(y.zone)
+                //   // console.log(r.zones[y.zone].tickets[r.zones[y.zone].defaultTicket].total)
+                // }
+                
                 for (let s of r.seats) {
+                  // for(let h of Object.values(r.zones)){
+                  //   if(h.id == s.zone){
+                  //     alert('id are the same')
+                  //   }
+                  // }
+
+                
+
+                  // for (let [name, value] of Object.entries(r.zones)) {
+                  //   console.log(name)
+                  //   console.log(value)
+                  // }
+                  // for (let h of r.zones){
+                  //   console.log(h)
+                  // }
+                  
+                  // console.log('h is'+h.id)
+                  // console.log('s is'+s.id)
+                  
+
+                  
                   let d = document.createElement("div");
                   
-                  d.onclick=()=>{
-                    console.log(s.available)
-                    let price = parseInt(p.price.minPrice + p.price.minPriceFee)
-                    thelist.push({
-                      eachPrice: price,
-                      seatLabel: s.label,
-                      dateTime: p.dates.performanceDate,
-                      ticketPrice: p.price.minPrice,
-                      fee: p.price.minPriceFee
-
-                    })
-                    // console.log('list is'+thelist)
-                    for (let li of thelist){
-                      console.log('price is'+li['eachPrice'])
-                      
-                    }
-                    console.log(total)
-
-                    // console.log(p.price.minPrice + p.price.minPriceFee)
-                    // console.log(s.id)
-                    // console.log(p.availabilityStatus)
-                    // console.log(s.zone)
+                  // for(let h of Object.values(r.zones)){
                     
-                
-                  }
+                  
                   d.classList.add("seat");
+                  
                   d.classList.add("Z" + s.zone);
-                  d.style.left = s.x / 2 + 500 + "px";
-                  d.style.top = s.y / 2 + 200 + "px";
+                  d.style.left = s.x / 2.5 + 500 + "px";
+                  d.style.top = s.y / 2.5 + 200 + "px";
+                  // for(i = 0;i<117;i++){
+                  //   let h = r.labels[`UYdv4OOeQCe/eq5R7cDGLQ/${i}`];
+                  //   console.log(h.x)
+                  // }
+                  for(let h of Object.values(r.labels)){
+                    let n = document.createElement('div');
+                    n.classList.add('name')
+                    n.style.left = h.x / 2.4 + 500 + "px";
+                    n.style.top = h.y / 2.5 + 196 + "px";
+                    n.innerText = h.content
+                    document.getElementById("seats").append(n);
+                  }
+                  // for(let h of r.labels){
+                  //   console.log
+                  // }
                   if (s.available) {
+                    // console.log(s.zone)
                     d.classList.add("available");
+                  }else{
+                    d.classList.add("unavailable");
+                  }
+                  d.onclick=()=>{
+                    
+                    // alert(h.tickets[h.defaultTicket].total)
+                    // let price = parseInt(p.price.minPrice + p.price.minPriceFee)
+                    // thelist.push({
+                    //   eachPrice: price,
+                    //   seatLabel: s.label,
+                    //   dateTime: p.dates.performanceDate,
+                    //   ticketPrice: p.price.minPrice,
+                    //   fee: p.price.minPriceFee
+
+                    // })
+                    // // console.log('list is'+thelist)
+                    // for (let li of thelist){
+                    //   console.log('price is'+li['eachPrice'])
+                      
+                    // }
+                    alert('ticket is'+r.zones[s.zone].defaultTicket)
+                    // alert(r.zones[s.zone].tickets[r.zones[s.zone].defaultTicket].total)
+                    
                   }
                   document.getElementById("seats").append(d);
-                }
+                  
+              
+            }
+
               });
           };
           cell.querySelector(".box").append(performanceDiv);
